479459e1c03b598ecbf44a40a433877d30447fda
tranngocbaobinh@gmail.com <tranngocbaobinh@gmail.com>
2024-04-01 09:21:21 +0700

    fix video

--- js/videoRender.js
+++ js/videoRender.js
@@ -93,9 +93,9 @@ const swiper = new Swiper(".swiper", {
   // },
 
   // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  // },
 });
 
 // function handleTouchMove(evt) {
